609a07be63dcdaa2da9b7a98e55180a5a9033b31
Paulo Giacomelli <paulohenriquegiacomelli@gmail.com>
2025-02-26 20:37:55 -0300

    Ultimo ex

---
+++ matrizes/ex16.7.c
@@ -0,0 +1,25 @@
+// Ler um vetor G de 13 elementos que contenha o gabarito da loteria esportiva codificado da seguinte
+// forma: 1-coluna um, 2-coluna do meio, 3-coluna dois.
+// Logo após, ler uma matriz 13 x 3 que contenha a aposta de um jogador. Considere que cada posição da
+// matriz armazenará o valor 1 se for apostado, 0 caso contrário. Calcular e escrever o número de pontos
+// obtidos pelo jogador. Escrever também o número de apostas simples, dupla ou tripla utilizadas pelo
+// apostador.
+
+#include <stdio.h>
+int main()
+{
+    int G[13][3] = {
+        {1, 0, 2},
+        {2, 1, 1},
+        {1, 1, 0},
+        {0, 1, 2},
+        {2, 0, 1},
+        {1, 0, 2},
+        {2, 1, 1},
+        {1, 1, 0},
+        {0, 1, 2},
+        {2, 0, 1},
+        {1, 0, 2},
+        {2, 1, 1},
+        {1, 1, 0}};
+}
